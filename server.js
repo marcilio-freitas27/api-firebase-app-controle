0fc4c94a80f382b1e10e12a4af665e18076c7b10
Marcilio Freitas <marcilio@beltsistemas.com.br>
2022-10-27 16:58:46 -0300

    second commit

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //import libraries
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as express from 'express';
-import * as bodyParser from "body-parser";
+// import * as functions from 'firebase-functions';
+// import * as admin from 'firebase-admin';
+// import * as express from 'express';
+// import * as bodyParser from "body-parser";
+
+const express = require('express');
 
 //initialize firebase inorder to access its services
-admin.initializeApp(functions.config().firebase);
+// admin.initializeApp(functions.config().firebase);
 
 //initialize express server
 const app = express();
@@ -13,31 +15,37 @@ const main = express();
 
 //add the path to receive request and set json as bodyParser to process the body 
 main.use('/api/v1', app);
-main.use(bodyParser.json());
-main.use(bodyParser.urlencoded({ extended: false }));
+main.use(express.json());
+main.use(express.urlencoded({ extended: false }));
 
 //initialize the database and the collection 
-const db = admin.firestore();
-const userCollection = 'users';
+// const db = admin.firestore();
+// const userCollection = 'users';
 
 //define google cloud function name
-export const webApi = functions.https.onRequest(main);
+// export const webApi = functions.https.onRequest(main);
 
 //get all users
-app.get('/users', async (req, res) => {
-    try {
-        const userQuerySnapshot = await db.collection(userCollection).get();
-        const users = [];
-        userQuerySnapshot.forEach(
-            (doc)=>{
-                users.push({
-                    id: doc.id,
-                    data:doc.data()
-            });
-            }
-        );
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
-});
+// app.get('/users', async (req, res) => {
+//     try {
+//         const userQuerySnapshot = await db.collection(userCollection).get();
+//         const users = [];
+//         userQuerySnapshot.forEach(
+//             (doc)=>{
+//                 users.push({
+//                     id: doc.id,
+//                     data:doc.data()
+//             });
+//             }
+//         );
+//         res.status(200).json(users);
+//     } catch (error) {
+//         res.status(500).send(error);
+//     }
+// });
+
+app.get("/", function(req, res) {
+    res.json('Conectado');
+})
+
+app.listen(8080, ()=> console.log('listen on http://localhost:8080'));

09176bd9c9f046446a95dd5fdc495a00c7b332b3
Marcilio Freitas <marcilio@beltsistemas.com.br>
2022-10-27 16:47:36 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+//import libraries
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as bodyParser from "body-parser";
+
+//initialize firebase inorder to access its services
+admin.initializeApp(functions.config().firebase);
+
+//initialize express server
+const app = express();
+const main = express();
+
+//add the path to receive request and set json as bodyParser to process the body 
+main.use('/api/v1', app);
+main.use(bodyParser.json());
+main.use(bodyParser.urlencoded({ extended: false }));
+
+//initialize the database and the collection 
+const db = admin.firestore();
+const userCollection = 'users';
+
+//define google cloud function name
+export const webApi = functions.https.onRequest(main);
+
+//get all users
+app.get('/users', async (req, res) => {
+    try {
+        const userQuerySnapshot = await db.collection(userCollection).get();
+        const users = [];
+        userQuerySnapshot.forEach(
+            (doc)=>{
+                users.push({
+                    id: doc.id,
+                    data:doc.data()
+            });
+            }
+        );
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
